71b672d1a38a5788d48d84cd1bc01ffaca50f2fa
Lorena Vazquez Esteban <lorena.esteban@outlook.com>
2024-09-04 10:58:40 -0600

    se agrega los proyectos de Negocio, ifraestructura para choferes

---
+++ Gen09-Parent/Gen09-Negocio/Gen09-Choferes/src/main/java/com/vazquez/choferes/Gen09ChoferesApplication.java
@@ -0,0 +1,13 @@
+package com.vazquez.choferes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gen09ChoferesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Gen09ChoferesApplication.class, args);
+	}
+
+}
---
+++ Gen09-Parent/Gen09-Negocio/Gen09-Choferes/src/test/java/com/vazquez/choferes/Gen09ChoferesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vazquez.choferes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Gen09ChoferesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
